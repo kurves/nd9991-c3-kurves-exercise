3d3514e3bc1c5d843d7e675d2f4c32675b59bf84
kurves <kavssimply@gmail.com>
2022-11-08 23:15:04 +0300

    initial commit

--- myApp.js
+++ myApp.js
@@ -1,12 +1,26 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
+app.get("/", (req,res)=>{
+    res.send("Hello Express");
+});
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/json", (req,res) => {
+    res.json({mesaage: "Hello Json"});
+});
 
 
-
-
-
-
+app.get('/now', (req,res)=>{
+    req.time = new Date().toString();
+    next();
+}, (req,res)=>{
+    res.json({time: "req.time"});
+})
 
 
 
