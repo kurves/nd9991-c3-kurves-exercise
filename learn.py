11a6babf2ec5bfd9533104e3543a2d12fe8977fd
kurves <kavssimply@gmail.com>
2022-11-09 22:14:06 +0300

    feat: add benefits function

--- learn.py
+++ learn.py
@@ -3,3 +3,10 @@ def list_benefits()
 
 def build_statements_info();
     return "is a benefit of cunctions"
+
+def name_the_benefits();
+    list_of_benefits = list_benefits();
+    for benefit in list_of-benefits:
+        print(build_sentence(benefit))
+
+name_the_benefits()
